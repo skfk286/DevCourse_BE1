4ddecb56989c3b2eedc443f6ecfee39f86332200
Yechan Jung <skfk286@github.com>
2024-07-25 12:04:41 +0900

    2024.07.25 수업 내용 정리 커밋

--- src/day_0725/test04/Test04.java
+++ src/day_0725/test04/Test04.java
@@ -4,15 +4,29 @@ import java.util.Arrays;
 
 public class Test04 {
 
+    public static int ha = 0;
+
     public static void main(String[] args) {
-        Student[] students = new Student[3];
+        Student[] students = new Student[9];
 
         students[0] = new Student("조믿음", 25, 95);
         students[1] = new Student("이준호", 22, 85);
         students[2] = new Student("양유진", 20, 100);
+        students[3] = new Student("조믿음", 25, 95);
+        students[4] = new Student("이준호", 22, 85);
+        students[5] = new Student("양유진", 20, 100);
+        students[6] = new Student("조믿음", 25, 95);
+        students[7] = new Student("이준호", 22, 85);
+        students[8] = new Student("양유진", 20, 100);
 
         // 람다식
-        Arrays.sort(students, (o1, o2) -> {return o1.getName().compareTo(o2.getName());}); // 이름 정렬
+        Arrays.sort(students, (o1, o2) -> {
+            int num = 100; // 람다도 하나의 메소드라서 안에서 필요한 변수 만들어 쓰는건 지장 없음
+            System.out.println("이름으로 정렬중..");
+            /** 주의. 람다 안에서는 student = new Student[10]; 과 같은 외부 변수를 변경해서는 안된다. */
+            ha = 200; // 클래스 맴버 변수는 변경 가능.
+            return o1.getName().compareTo(o2.getName());
+        }); // 이름 정렬
         
         for (int i = 0; i < students.length; i++) {
             System.out.println(students[i]);
