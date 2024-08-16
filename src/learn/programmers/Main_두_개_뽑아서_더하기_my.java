0dd575b3c9679ee4992e854d0164b38ad2287079
Yechan Jung <skfk286@hanmail.net>
2024-08-16 20:49:44 +0900

    알고리즘 문제풀이 - 프로그래머스 책 이해

--- src/learn/programmers/Main_두_개_뽑아서_더하기_my.java
+++ src/learn/programmers/Main_두_개_뽑아서_더하기_my.java
@@ -1,38 +1,49 @@
 package learn.programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main_두_개_뽑아서_더하기_my {
 
+    // 나의 문제풀이
     public static int[] solution(int[] numbers) {
         int[] answer = {};
 
         List<Integer> list = new ArrayList<>();
 
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=i+1; j<numbers.length; j++) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
 
                 int add = numbers[i] + numbers[j];
 
-                if(!list.contains(add)) {
+                if (!list.contains(add)) {
                     list.add(add);
                 }
             }
         }
 
         answer = list.stream().sorted().
-                        mapToInt(Integer::intValue).toArray();
+                mapToInt(Integer::intValue).toArray();
 
         return answer;
     }
 
+    // 프로그래머스 책 풀이
+    public static int[] solution2(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+
     public static void main(String[] args) {
-        int[] numbers = {2,1,3,4,1};
+        int[] numbers = {2, 1, 3, 4, 1};
 //        int[] numbers = {5,0,2,7};
 
-        int[] result = solution(numbers);
+        int[] result = solution2(numbers);
 
         System.out.println(Arrays.toString(result));
 
